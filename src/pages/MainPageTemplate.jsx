3123c6f7472186f034ecdb19d983a4cd8794beec
crowwan <crowwan@naver.com>
2023-02-26 23:39:53 +0900

    feat: 전체 페이지 탬플릿 컴포넌트 생성

--- src/pages/MainPageTemplate.jsx
+++ src/pages/MainPageTemplate.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import styled from "styled-components";
+import Header from "../layouts/Header";
 import Nav from "../layouts/Nav";
+import Main from "../layouts/Main";
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  height: 100vh;
+`;
 function MainPageTemplate() {
+  const location = useLocation();
   return (
-    <div>
-      <Nav />
-      <Outlet />
-    </div>
+    <StyledContainer>
+      <Header />
+      <Nav selected={location.pathname.slice(-1)} />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledContainer>
   );
 }
 
