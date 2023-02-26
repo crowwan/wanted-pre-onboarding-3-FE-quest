f6169d5e459c2e834b1a44796f78d566506f7aba
crowwan <crowwan@naver.com>
2023-02-26 21:12:56 +0900

    feat: 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import MainPageTemplate from "./pages/MainPageTemplate";
+import Page from "./pages/Page";
+import PageA from "./pages/PageA";
+import PageB from "./pages/PageB";
+import PageC from "./pages/PageC";
+import SignIn from "./pages/SignIn";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="/page" element={<MainPageTemplate />}>
+        <Route path="A" element={<PageA />} />
+        <Route path="B" element={<PageB />} />
+        <Route path="C" element={<PageC />} />
+        <Route path=":page" element={<Page />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
