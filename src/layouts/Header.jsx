08a6c73be9f34e6beadc511b17f6aa1ccc3045b6
crowwan <crowwan@naver.com>
2023-02-26 23:38:07 +0900

    feat: 공통 해더 컴포넌트 생성

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  padding: 2rem;
+  font-size: 1.5rem;
+  font-weight: bold;
+  width: 100%;
+  height: 10%;
+  border-bottom: 1px solid black;
+`;
 
 function Header() {
-  return <div>Header</div>;
+  return <StyledHeader>Wanted Pre-onboarding course</StyledHeader>;
 }
 
 export default Header;
