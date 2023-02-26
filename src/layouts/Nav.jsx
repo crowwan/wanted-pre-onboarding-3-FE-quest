b728c437cbea24c302c8f3c47857e9d0a0079b1c
crowwan <crowwan@naver.com>
2023-02-26 23:39:00 +0900

    feat: 네비게이션 바 컴포넌트 생성

--- src/layouts/Nav.jsx
+++ src/layouts/Nav.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import PageItem from "../components/PageItem";
 
-function Nav() {
-  return <div>Nav</div>;
+const StyledNav = styled.nav`
+  padding: 2rem;
+  font-size: 1.3rem;
+  font-weight: bold;
+  width: 20%;
+  height: 90%;
+  border-right: 1px solid black;
+`;
+
+const StyledNavItemContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+function Nav({ selected }) {
+  const [pages, setPages] = useState(["Page A", "Page B", "Page C"]);
+  return (
+    <StyledNav>
+      <StyledNavItemContainer>
+        {pages.map((page) => (
+          <PageItem
+            key={page}
+            pageName={page}
+            selected={selected === page.slice(-1)}
+          />
+        ))}
+      </StyledNavItemContainer>
+    </StyledNav>
+  );
 }
 
 export default Nav;
